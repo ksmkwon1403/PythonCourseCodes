31554e29b670276d06b2f1abe4c1914e769156a5
Seungmin <seungmin.kwon3@gmail.com>
2021-12-22 22:41:59 +0900

    1

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def plus(a, b):
-    if type(b) is str: # str
-        return None
-    else:
+    if type(b) == int or type(b) == float:
         return a + b
+    else:
+        return None
 
-print(plus(12, "10"))
+print(plus(1, 3.12351))
